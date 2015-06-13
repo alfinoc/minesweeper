c07558b52f213d975b14ef69d23fb6ea8e685744
alfinoc <chrisalfino@gmail.com>
2015-06-13 00:46:11 -0700

    removed the last of the python-isms

--- jsport/game.js
+++ jsport/game.js
@@ -14,71 +14,48 @@ Board.HIDDEN_VAL = -2
 Board.MARKED_VAL = -3
 
 Board.DISPLAY = {};
-Board.DISPLAY[Board.MINE_VAL] = ['red', 'X'];
+Board.DISPLAY[Board.MINE_VAL] = ['red', '&circledast;'];
 Board.DISPLAY[Board.HIDDEN_VAL] = ['gray', '&square;'];
-Board.DISPLAY[Board.MARKED_VAL] = ['pink', '!'];
+Board.DISPLAY[Board.MARKED_VAL] = ['pink', '&And;'];
 Board.DISPLAY[0] = ['gray', ' '];
 
-function len(a) {
-   return a.length;
-}
-
-function int(s) {
-   return parseInt(s);  
-}
-
-function map(fn, array) {
-   return array.map(fn);
-}
-
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
+   var currentIndex = array.length, temporaryValue, randomIndex;
+   while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+   }
   return array;
 }
-
-var range = xrange = function(low, hi) {
-   var res = [];
-   for (var i = low; i < hi; i++) {
-      res.push(i);
-   }
-   return res;
-};
 
 Board.init = function(mines, width, height) {
    Board.width = width;
    Board.height = height;
-   Board.grid = [];
+   var grid = [];
    for (var i = 0; i < mines; i++)
-      Board.grid.push(Board.MINE_VAL);
+      grid.push(Board.MINE_VAL);
    for (var i = 0; i < width * height - mines; i++)
-      Board.grid.push(0);
-   shuffle(Board.grid);
-   Board.grid = map(Board._computeHint, range(0, len(Board.grid)));
+      grid.push(0);
+   shuffle(grid);
+   for (var i = 0; i < grid.length; i++)
+      grid[i] = Board._computeHint(grid, i);
+   Board.grid = grid;
 }
 
 Board.hint = function(i) {
    return Board.grid[i];
 }
 
-Board._computeHint = function(index) {
-   if (Board.grid[index] == Board.MINE_VAL) {
+Board._computeHint = function(grid, index) {
+   if (grid[index] == Board.MINE_VAL) {
       return Board.MINE_VAL;
    } else {
-      var pair = Board.toCoordinates(index);
-      var x = pair[0];
-      var y = pair[1];
       var adjacent = 0;
       Board.adjacent(index).forEach(function(i) {
-         adjacent += Board.grid[i] == Board.MINE_VAL ? 1 : 0;
+         adjacent += grid[i] == Board.MINE_VAL ? 1 : 0;
       });
       return adjacent;
    }
@@ -87,40 +64,53 @@ Board._computeHint = function(index) {
 Board.format = function(index, hint) {
    hint = parseInt(hint);
    var d = Board.DISPLAY[hint];
-   var color, character;
-   if (d) {
-      color = d[0];
-      character = d[1];
-   } else {
-      color = Color.toColor(hint);
-      character = hint;
-   }
+   var color = d ? d[0] : Color.toColor(hint);
+   var character = d ? d[1] : hint;
+   return [color, character];
+};
 
-   return '<td color="' + color + '" index="' + index + '">' + character + '</td>';
+Board.element = function(index, hint) {
+   var format = Board.format(index, hint);
+   var td = document.createElement('td');
+   td.setAttribute('color', format[0]);
+   td.setAttribute('index', index);
+   td.innerHTML = format[1];
+   return td;
 };
 
 Board.dump = function(hideMask, markMask) {
    var parent = document.querySelector(TABLE_SELECTOR);
-   var masked = Board.masked(hideMask, markMask)
-   var display = function(index) { return masked[index]; };
-   parent.innerHTML = '';
-   for (var i = 0; i < Board.height; i++) {
-      var row = '<tr>';
-      for (var j = 0; j < Board.width; j++) {
-         var index = j + i * Board.width;
-         row += Board.format(index, masked[index]);
+   var masked = Board.masked(hideMask, markMask);
+   if (parent.innerHTML.trim().length == 0) {
+      // Reload a fresh table of 'td' elements.
+      parent.innerHTML = '';
+      for (var i = 0; i < Board.height; i++) {
+         var row = document.createElement('tr');
+         for (var j = 0; j < Board.width; j++) {
+            var index = j + i * Board.width;
+            row.appendChild(Board.element(index, masked[index]));
+         }
+         parent.appendChild(row);
       }
-      parent.innerHTML += row + '</tr>';
+   } else {
+      // Update existing 'td' elements.
+      var cells = document.querySelectorAll('td');
+      for (var i = 0; i < cells.length; i++) {
+         var element = cells[i];
+         var index = parseInt(element.getAttribute('index'));
+         var format = Board.format(index, masked[index])
+         element.setAttribute('color', format[0]);
+         element.innerHTML = format[1];
+      }
    }
 };
 
 Board.masked = function(hideMask, markMask) {
    function apply(original, mask, replace) {
-      var indices = xrange(0, len(original));
-      function swap(i) {
-         return mask != undefined && mask[i] ? replace : original[i];
-      }
-      return map(swap, indices);
+      var res = [];
+      for (var i = 0; i < original.length; i++)
+         res.push(mask != undefined && mask[i] ? replace : original[i]);
+      return res;
    }
 
    var res = apply(Board.grid, hideMask, Board.HIDDEN_VAL)
@@ -137,7 +127,7 @@ Board.adjacent = function(index) {
       for (var j = Math.max(0, y - 1); j < Math.min(y + 2, Board.height); j++)
          if (!(i == x && j == y))
             pairs.push([i, j]);
-   return map(Board.toIndex, pairs);
+   return pairs.map(Board.toIndex);
 }
 
 Board.toIndex = function(pair) {
@@ -145,7 +135,7 @@ Board.toIndex = function(pair) {
 }
 
 Board.toCoordinates = function(i) {
-   return [i % Board.width, int(i / Board.width)];
+   return [i % Board.width, parseInt(i / Board.width)];
 };
 
 Minesweeper = {};
@@ -153,20 +143,18 @@ Minesweeper = {};
 Minesweeper.init = function(mines, width, height) {
    Board.init(mines, width, height);
    Minesweeper.hidden = []
-   for (var i = 0; i < len(Board.grid); i++) {
+   for (var i = 0; i < Board.grid.length; i++)
       Minesweeper.hidden.push(true);
-   }
 };
 
 Minesweeper.reveal = function(i) {
-   if (!Minesweeper.hidden[i]) {
+   if (!Minesweeper.hidden[i])
       return false;
-   }
    Minesweeper.hidden[i] = false;
    var hit = Board.hint(i);
-   if (hit == Board.MINE_VAL) {
+   if (hit == Board.MINE_VAL)
       return true;
-   } else if (hit == 0) {
+   else if (hit == 0) {
       Board.adjacent(i).forEach(function(j) {
          Minesweeper.reveal(j);
       });
@@ -184,11 +172,11 @@ Player.init = function() {
    Minesweeper.init(100, 30, 30);
    Player.errors = 0;
    Player.marked = [];
-   for (var i = 0; i < len(Board.grid); i++) {
+   for (var i = 0; i < Board.grid.length; i++)
       Player.marked.push(false);
-   }
    document.querySelector(TABLE_SELECTOR).addEventListener('click', function(evt) {
-      Player.reveal(parseInt(evt.srcElement.getAttribute('index')));
+      var action = event.shiftKey ? Player.mark : Player.reveal;
+      action(parseInt(evt.srcElement.getAttribute('index')));
       Player.dump();
    });
 };
@@ -197,11 +185,9 @@ Player.complete = function(self) {
    function mismatched(mark, hide, val) {
       return hide && ((val == Board.MINE_VAL) != mark);
    }
-   for (var i = 0; i < Player.marked.length; i++) {
-      if (mismatched(Player.marked[i], Game.hidden[i], Board.grid[i])) {
+   for (var i = 0; i < Player.marked.length; i++)
+      if (mismatched(Player.marked[i], Game.hidden[i], Board.grid[i]))
          return false;
-      }
-   }
    return true;
 };
 
@@ -225,14 +211,14 @@ Player.state = function() {
 
 Player.sweep = function() {
    function mines(hint, adjacent, marked, unknown) {
-      return len(unknown) == hint - len(marked) ? unknown : [];
+      return unknown.length == hint - marked.length ? unknown : [];
    }
    return Player.infer(mines, partial(Player.mark, check=true));
 };
 
 Player.eliminate = function() {
    function safe(hint, adjacent, marked, unknown) {
-      return hint - len(marked) == 0 ? unknown : [];
+      return hint - marked.length == 0 ? unknown : [];
    }
    return Player.infer(safe, Player.reveal);
 };
@@ -242,10 +228,10 @@ Player.eliminate = function() {
 // board. 'applyFn' is only called after all 'selectFn' calls have been
 // completed. returns true if anything was applied, false otherwise.
 Player.infer = function(selectFn, applyFn) {
-   matching = []
+   var matching = [];
+   var state = Player.state();
 
-   state = Player.state()
-   for (var i = 0; i < len(Player.marked); i++) {
+   for (var i = 0; i < Player.marked.length; i++) {
       var hint = state[i];
 
       // Only consider useful hints that are known.
@@ -257,7 +243,7 @@ Player.infer = function(selectFn, applyFn) {
       var marked = adjacent.filter(function(j) {
          return state[j] == Board.MARKED_VAL || state[j] == Board.MINE_VAL;
       });
-      var unknown = adjacent.filter(function(j){
+      var unknown = adjacent.filter(function(j) {
          return state[j] == Board.HIDDEN_VAL;
       });
 
@@ -266,19 +252,21 @@ Player.infer = function(selectFn, applyFn) {
       });
    }
    matching.forEach(applyFn(i));
-   return len(matching) != 0;
+   return matching.length != 0;
 };
 
 Player.guess = function() {
    var state = Player.state()
-   var unknown = xrange(0, len(Player.marked)).filter(function(i) {
-      return state[i] == Board.HIDDEN_VAL;
-   });
-   if (len(unknown) == 0)
+   var unknown = [];
+   for (var i = 0; i < Player.marked.length; i++)
+      if (state[i] == Board.HIDDEN_VAL)
+         unknown.push(i);
+   if (unknown.length == 0)
       return false;
    return Player.reveal(unknown[parseInt(Math.random() * unknown.length)]);
 }
 
+// Disable selection on the entire page.
 function disableselect(e) { return false; }
 function reEnable() { return true; }
 document.onselectstart = new Function('return false');
@@ -286,4 +274,3 @@ if (window.sidebar) {
    document.onmousedown = disableselect;
    document.onclick = reEnable;
 }
-
